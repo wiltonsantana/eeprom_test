fc36998d442a8c0d8b25e2390b5b3f764b5a09f5
Wilton Santana <wpss@cesar.org.br>
2017-07-21 16:20:40 -0300

    Make some improves on Light_EEPROM file

--- Light_EEPROM_v2.ino
+++ Light_EEPROM_v2.ino
@@ -1,13 +1,13 @@
 #include <KNoTThing.h>
 
 /*
- * Copyright (c) 2016, CESAR.
- * All rights reserved.
- *
- * This software may be modified and distributed under the terms
- * of the BSD license. See the LICENSE file for details.
- *
- */
+   Copyright (c) 2016, CESAR.
+   All rights reserved.
+
+   This software may be modified and distributed under the terms
+   of the BSD license. See the LICENSE file for details.
+
+*/
 
 #include <KNoTThing.h>
 #include <SPI.h>
@@ -27,18 +27,16 @@
 #define WRSR  0b00000001
 #define RDSR  0b00000101
 
-#define ZEROS 0b00000000
-#define DATA  0b01010100
-#define ADDR  0b0000000000011010
-
-bool ctr = false;
-byte status_reg = 0b11100000;
+#define ZEROS   0b00000000
+#define DATA    0b01010100
+#define STATUS  0b11100000
+#define ADDR    0b0000000000011010
 
 KNoTThing thing;
 
-void eeprom_spi_write_enable(){
+void eeprom_spi_write_enable() {
   SPI.beginTransaction(SPISettings(5000000, MSBFIRST, SPI_MODE0));
-  
+
   Serial.println("Setting Enable Writing");
 
   digitalWrite(CHIP_SELECT, LOW);
@@ -46,11 +44,11 @@ void eeprom_spi_write_enable(){
   SPI.transfer(WREN);
   interrupts();
   digitalWrite(CHIP_SELECT, HIGH);
-   
+
   SPI.endTransaction();
 }
 
-void eeprom_spi_write_disable(){
+void eeprom_spi_write_disable() {
   SPI.beginTransaction(SPISettings(5000000, MSBFIRST, SPI_MODE0));
 
   Serial.println("Disable Writing");
@@ -60,14 +58,14 @@ void eeprom_spi_write_disable(){
   SPI.transfer(WRDI);
   interrupts();
   digitalWrite(CHIP_SELECT, HIGH);
-  
+
   SPI.endTransaction();
 }
 
-void eeprom_spi_read_status(){
+void eeprom_spi_read_status() {
   SPI.beginTransaction(SPISettings(5000000, MSBFIRST, SPI_MODE0));
   byte reg_data;
-  
+
   digitalWrite(CHIP_SELECT, LOW);
   noInterrupts();
   SPI.transfer(RDSR);
@@ -78,11 +76,11 @@ void eeprom_spi_read_status(){
 
   Serial.print("STATUS REGISTER: ");
   Serial.println(reg_data, BIN);
-  
+
   SPI.endTransaction();
 }
 
-void eeprom_spi_write_status(byte status_reg){
+void eeprom_spi_write_status(byte status_reg) {
   SPI.beginTransaction(SPISettings(5000000, MSBFIRST, SPI_MODE0));
 
   Serial.println("Enable Writing");
@@ -94,18 +92,18 @@ void eeprom_spi_write_status(byte status_reg){
   SPI.transfer(status_reg);
   interrupts();
   digitalWrite(CHIP_SELECT, HIGH);
-  
+
   SPI.endTransaction();
 }
 
 
-void eeprom_spi_read_reg(word addr){
+void eeprom_spi_read_reg(word addr) {
   byte data_reg;
 
   SPI.beginTransaction(SPISettings(5000000, MSBFIRST, SPI_MODE0));
 
   Serial.println("Reading Register");
-  
+
   digitalWrite(CHIP_SELECT, LOW);
   noInterrupts();
   SPI.transfer(READ);
@@ -117,11 +115,11 @@ void eeprom_spi_read_reg(word addr){
 
   Serial.print("REGISTER Value: ");
   Serial.println(data_reg, BIN);
-  
+
   SPI.endTransaction();
 }
 
-void eeprom_spi_write_reg(word addr, byte data){
+void eeprom_spi_write_reg(word addr, byte data) {
   SPI.beginTransaction(SPISettings(5000000, MSBFIRST, SPI_MODE0));
 
   Serial.println("Writing on Register");
@@ -129,7 +127,7 @@ void eeprom_spi_write_reg(word addr, byte data){
   Serial.println(addr, BIN);
   Serial.print("Data: ");
   Serial.println(data, BIN);
-  
+
   digitalWrite(CHIP_SELECT, LOW);
   noInterrupts();
   SPI.transfer(WRITE);
@@ -138,117 +136,112 @@ void eeprom_spi_write_reg(word addr, byte data){
   SPI.transfer(data);
   interrupts();
   digitalWrite(CHIP_SELECT, HIGH);
-  
+
   SPI.endTransaction();
 }
 
 static int light_read(uint8_t *val)
 {
-    *val = digitalRead(LIGHT_BULB_PIN);
-//    Serial.print(F("Light Status: "));
-//    if (*val)
-//      Serial.println(F("ON"));
-//    else
-//      Serial.println(F("OFF"));
-    return 0;
+  *val = digitalRead(LIGHT_BULB_PIN);
+  //    Serial.print(F("Light Status: "));
+  //    if (*val)
+  //      Serial.println(F("ON"));
+  //    else
+  //      Serial.println(F("OFF"));
+  return 0;
 }
 
 static int light_write(uint8_t *val)
 {
-    digitalWrite(LIGHT_BULB_PIN, *val);
-//    Serial.print(F("Light Status: "));
-//    if (*val)
-//      Serial.println(F("ON"));
-//    else
-//      Serial.println(F("OFF"));
-      /* TODO: Save light status in EEMPROM in to handle when reboot */
-    return 0;
+  digitalWrite(LIGHT_BULB_PIN, *val);
+  //    Serial.print(F("Light Status: "));
+  //    if (*val)
+  //      Serial.println(F("ON"));
+  //    else
+  //      Serial.println(F("OFF"));
+  /* TODO: Save light status in EEMPROM in to handle when reboot */
+  return 0;
 }
 
 void setup()
 {
-    Serial.begin(9600);
+  Serial.begin(9600);
 
-    pinMode(LIGHT_BULB_PIN, OUTPUT);
-    /* TODO: Read lamp status from eeprom for reboot cases */
-    thing.init("KNoTThing");
-    thing.registerBoolData(LIGHT_BULB_NAME, LIGHT_BULB_ID, KNOT_TYPE_ID_SWITCH,
-        KNOT_UNIT_NOT_APPLICABLE, light_read, light_write);
+  pinMode(LIGHT_BULB_PIN, OUTPUT);
+  /* TODO: Read lamp status from eeprom for reboot cases */
+  thing.init("KNoTThing");
+  thing.registerBoolData(LIGHT_BULB_NAME, LIGHT_BULB_ID, KNOT_TYPE_ID_SWITCH,
+                         KNOT_UNIT_NOT_APPLICABLE, light_read, light_write);
 
-    thing.registerDefaultConfig(LIGHT_BULB_ID);
+  thing.registerDefaultConfig(LIGHT_BULB_ID);
 
-    Serial.println(F("Remote Light Bulb KNoT Demo"));
+  Serial.println(F("Remote Light Bulb KNoT Demo"));
 
-    pinMode(WRITE_PROTECT_PIN, OUTPUT);
-    digitalWrite(WRITE_PROTECT_PIN, HIGH);  // give write permission
+  pinMode(WRITE_PROTECT_PIN, OUTPUT);
+  digitalWrite(WRITE_PROTECT_PIN, HIGH);  // give write permission
 
-    pinMode(HOLD_PIN, OUTPUT);
-    digitalWrite(HOLD_PIN, HIGH); // disable hold so i can perform operations
-    
-     SPI.begin();
-     pinMode(CHIP_SELECT, OUTPUT);
-     digitalWrite(CHIP_SELECT, HIGH);
+  pinMode(HOLD_PIN, OUTPUT);
+  digitalWrite(HOLD_PIN, HIGH); // disable hold so i can perform operations
+
+  SPI.begin();
+  pinMode(CHIP_SELECT, OUTPUT);
+  digitalWrite(CHIP_SELECT, HIGH);
 }
 
 
 void loop()
 {
 
-    thing.run();
+  thing.run();
 
-    char std = '0';
-  
-    if (!ctr){
-        Serial.println("MENU:");
-        Serial.println("0 - Display STATUS register");
-        Serial.println("1 - Display ENABLE Writing");
-        Serial.println("2 - Display WRITES in a register");
-        Serial.println("3 - Display DISABLE Writing");
-        delay(1000);
-        
-        ctr = true;
-      }
-      
-     if(Serial.available()){
-       std = Serial.read();    
-      }
+  char std = '0';
 
-    //digitalWrite(HOLD_PIN, HIGH);
-    switch(std)
-    {
-      case '0':
-        eeprom_spi_read_status();
-        eeprom_spi_write_enable();
-        eeprom_spi_write_status(status_reg);
-        eeprom_spi_write_enable();
-        eeprom_spi_write_reg(ADDR,DATA);
-        eeprom_spi_read_reg(ADDR);
-        std='1';
-        break;
-      case '1':
-        eeprom_spi_write_enable();
-        eeprom_spi_write_status(status_reg);
-        std='2';
-        break;
-      case '2':
-        eeprom_spi_write_enable();
-        eeprom_spi_write_reg(ADDR,DATA);
-        eeprom_spi_read_reg(ADDR);
-        Serial.println("REMEBER: After a write operation the WRITING IS DISABLE");
-        std = '0';
-        break;
-      case '3':
-        eeprom_spi_write_disable();
-        std = '0';
-        break;
-        
-      default:
-        
-        ctr = false; 
-        std = '0';
-                
-        break;
-        
-    }
-    //digitalWrite(HOLD_PIN, LOW);  
+  Serial.println("EEPROM MENU:");
+  Serial.println("0 - Display STATUS register");
+  Serial.println("1 - Display ENABLE Writing");
+  Serial.println("2 - Display WRITES in a register");
+  Serial.println("3 - Display DISABLE Writing");
+
+  if (Serial.available()) {
+    std = Serial.read();
+  }
+
+  //digitalWrite(HOLD_PIN, HIGH);
+  switch (std)
+  {
+    case '0':
+      eeprom_spi_read_status();
+      std = '1';
+      break;
+    case '1':
+      eeprom_spi_write_enable();
+      eeprom_spi_write_status(STATUS);
+      std = '2';
+      break;
+    case '2':
+      eeprom_spi_write_enable();
+      eeprom_spi_write_reg(ADDR, DATA);
+      eeprom_spi_read_reg(ADDR);
+      Serial.println("REMEBER: After a write operation the WRITING IS DISABLE");
+      std = '0';
+      break;
+    case '3':
+      eeprom_spi_write_disable();
+      std = '0';
+      break;
+
+    default:
+
+      Serial.println("MENU:");
+      Serial.println("0 - Display STATUS register");
+      Serial.println("1 - Display ENABLE Writing");
+      Serial.println("2 - Display WRITES in a register");
+      Serial.println("3 - Display DISABLE Writing");
+
+      std = '0';
+
+      break;
+
+  }
+  //digitalWrite(HOLD_PIN, LOW);
 }
